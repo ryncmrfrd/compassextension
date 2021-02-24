1395993276b31b080ab661cd22855ca596546a17
Ryan Comerford <lordfennec@gmail.com>
2021-02-25 00:09:42 +1100

    Finish calendar chevrons/top bar + shuffle centrecontent.css

--- bin/centreContent.js
+++ bin/centreContent.js
@@ -6,18 +6,12 @@
 let btnmore = document.querySelector("div#panel-1072-innerCt div a");
 btnmore ? btnmore.innerText = "See More" : "";
 
-let calendarBtnClasses = ["fa-chevron-left", "fa-chevron-right"];
-if(true){
-    // document.querySelectorAll("#calendardaywidget-1013-tb-prev span span, #calendardaywidget-1013-tb-next span span").forEach((el,i) => {
-    //     el.classList.add(...["fas", calendarBtnClasses[i]]);
-    //     el.classList.remove(...["x-btn-inner","x-tbar-page-prev","x-tbar-page-next"]);
-    // });
+// change forward/backward calendar chevrons
+let calendarBtns = document.querySelectorAll("#calendardaywidget-1013-tb-prev > span > span, #calendardaywidget-1013-tb-next > span > span"),
+	calendarBtnClasses = ["fa-chevron-left", "fa-chevron-right"];
 
-    let prev = document.querySelector("#calendardaywidget-1013-tb-prev span span")
-    prev.classList.add(...["fas", "fa-chevron-left"]);
-    prev.innerHTML = '';
-
-    let next = document.querySelector("#calendardaywidget-1013-tb-next span span")
-    next.classList.add(...["fas", "fa-chevron-right"]);
-    next.innerHTML = '';
-}
+calendarBtns.forEach((elem, i) => {
+	console.log(calendarBtnClasses[i])
+	elem.classList.add(...["fas", calendarBtnClasses[i]]);
+	elem.innerHTML = '';
+});
